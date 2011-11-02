f5787a23cf27021549e5fd0d67141cddaf09bf60
Ben George <ben@whitepixels.com.au>
2011-11-02 21:15:45 +1100

    Working on renderer

--- wp-facebook-ogp/class-ogp-render.php
+++ wp-facebook-ogp/class-ogp-render.php
@@ -15,6 +15,21 @@ class OgpRenderOgp {
 	var $_post;
 	
 	/**
+	 * @var Array of output formats for this renderer
+	 */
+	var $_formats = array(
+		'sitename'		=> "\t<meta property='og:site_name' content='%s' />\n",
+		'admins'		=> "\t<meta property='fb:admins' content='%s' />\n",
+		'app_id'		=> "\t<meta property='fb:app_id' content='%s' />\n",
+		'page_id'		=> "\t<meta property='fb:page_id' content='%s' />\n",
+		'url'			=> "\t<meta property='og:url' content='%s' />\n",
+		'title'			=> "\t<meta property='og:title' content='%s' />\n",
+		'description'	=> "\t<meta property='og:description' content='%s' />\n",
+		'type'			=> "\t<meta property='og:type' content='%s' />\n",
+		'image'			=> "\t<meta property='og:image' content='%s' />\n",
+	)
+	
+	/**
 	 * PHP4 style Constructor - Calls PHP5 Style Constructor
 	 *
 	 * @return WP_Http
@@ -38,9 +53,28 @@ class OgpRenderOgp {
 		return $this;
 	}
 	
+	function getAllMeta(){
+		$_output = '';
+		foreach ($this->_modelData as $itemKey => $itemValue){
+			switch ($itemKey){				
+				case 'images':
+					if(array_key_exists('image', $this->_formats)){
+						foreach($itemValue as $image){
+							$_output .= sprintf($this->_formats['image'], $mage);							
+						}						
+					}								
+					break;
+				default:
+					if(array_key_exists($itemKey, $this->_formats)){
+						$_output .= sprintf($this->_formats[$itemKey], $itemValue);
+					}
+			}
+		}
+	}
+	
 	function getSiteName(){
 		if(array_key_exists('sitename', $this->_model))
-			return "\t<meta property='og:site_name' content='" . $this->_model['sitename'] . "' />\n";
+			return sprintf($this->_formats['sitename'], $this->_model['sitename']);
 		return false;
 	}
 	
